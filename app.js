cbd5141703bf46300dc57e7a09a7a82bcdcade8e
ThaiCoder2003 <101780284+ThaiCoder2003@users.noreply.github.com>
2024-11-04 15:21:29 +0700

    Complete Page

--- app.js
+++ app.js
@@ -3,15 +3,28 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const expressLayouts = require('express-ejs-layouts');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var coffeeRouter = require('./routes/coffee');
+var shopRouter = require('./routes/shop');
+var detailRouter = require('./routes/detail');
+var aboutRouter = require('./routes/about');
+var blogRouter = require('./routes/blog');
+var contactRouter = require('./routes/contact');
+var loginRouter = require('./routes/login');
+var registerRouter = require('./routes/register')
 
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+
+app.use(expressLayouts);
+
+// Set the default layout file
+app.set('layout', 'layouts/layout');
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -20,7 +33,14 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/coffee', coffeeRouter);
+app.use('/shop', shopRouter);
+app.use('/detail', detailRouter);
+app.use('/about', aboutRouter);
+app.use('/blog', blogRouter);
+app.use('/contact', contactRouter);
+app.use('/login', loginRouter);
+app.use('/register', registerRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
